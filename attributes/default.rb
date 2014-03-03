9abc35b73f383d6aff2646bb37e48e4481b9bfc6
Ai Makabi <a.macbee@gmail.com>
2014-03-04 08:24:58 +0900

    recipeを実行するためのattributeファイルを作成

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default["python"]["version"] = "2.7.6"
+default["python"]["checksum"] = "1d8728eb0dfcac72a0fd99c17ec7f386"
+default['python']['prefix_dir'] = '/usr/local'
+default["python"]["configure_options"] = "--prefix=#{python['prefix_dir']}"
+default['python']['url'] = 'http://www.python.org/ftp/python'
